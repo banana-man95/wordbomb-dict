8cc992e8e08ea04e7c69de6f25426ca1d1a3ca18
death <63429751+banana-man95@users.noreply.github.com>
2024-07-31 17:21:16 -0400

    Update infyieldedit.lua

--- infyieldedit.lua
+++ infyieldedit.lua
@@ -12422,59 +12422,30 @@ addcmd('thawunanchored',{'thawua','unfreezeunanchored','unfreezeua'},function(ar
     frozenParts = {}
 end)
 
-local CollisionGroup = game:GetService("PhysicsService"):CreateCollisionGroup("UnanchoredObjects")
-
 addcmd('tpunanchored',{'tpua'},function(args, speaker)
     local players = getPlayer(args[1], speaker)
-    local unanchoredParts = {}
-
     for i,v in pairs(players) do
         local Forces = {}
         for _,part in pairs(workspace:GetDescendants()) do
-            if Players[v].Character:FindFirstChild('Head') and 
-               part:IsA("BasePart") and 
-               part.Anchored == false and 
-               not part:IsDescendantOf(speaker.Character) and 
-               part.Name ~= "Torso" and 
-               part.Name ~= "Head" and 
-               part.Name ~= "Right Arm" and 
-               part.Name ~= "Left Arm" and 
-               part.Name ~= "Right Leg" and 
-               part.Name ~= "Left Leg" and 
-               part.Name ~= "HumanoidRootPart" then
-
+            if Players[v].Character:FindFirstChild('Head') and part:IsA("BasePart" or "UnionOperation" or "Model") and part.Anchored == false and not part:IsDescendantOf(speaker.Character) and part.Name == "Torso" == false and part.Name == "Head" == false and part.Name == "Right Arm" == false and part.Name == "Left Arm" == false and part.Name == "Right Leg" == false and part.Name == "Left Leg" == false and part.Name == "HumanoidRootPart" == false then
                 for i,c in pairs(part:GetChildren()) do
                     if c:IsA("BodyPosition") or c:IsA("BodyGyro") then
                         c:Destroy()
                     end
                 end
-
                 local ForceInstance = Instance.new("BodyPosition")
                 ForceInstance.Parent = part
                 ForceInstance.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
                 table.insert(Forces, ForceInstance)
-
-                table.insert(unanchoredParts, part)
-
-                part.CollisionGroup = "UnanchoredObjects"
-
-                if not table.find(frozenParts, part) then
-                    table.insert(frozenParts, part)
+                if not table.find(frozenParts,part) then
+                    table.insert(frozenParts,part)
                 end
             end
         end
-
         for i,c in pairs(Forces) do
             c.Position = Players[v].Character.Head.Position
         end
     end
-
-    for _, part in ipairs(unanchoredParts) do
-        PhysicsService:SetPartCollisionGroup(part, "UnanchoredObjects")
-    end
-
-    PhysicsService:CollisionGroupSetCollidable("UnanchoredObjects", "Default", false)
-    PhysicsService:CollisionGroupSetCollidable("UnanchoredObjects", "UnanchoredObjects", true)
 end)
 
 local orbitConnection
